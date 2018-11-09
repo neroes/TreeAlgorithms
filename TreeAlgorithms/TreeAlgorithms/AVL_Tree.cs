517755a78c40731b9be4f9ce7e4ba95f92ef56ee
Soren Jesper Bloch <sorenjesperbloch@msn.com>
2018-11-09 15:40:30 +0100

    initial code with some of the implementation of AVL big issue, i don't know how to make the Node children of the inherited class the same as the inherited class

---
+++ TreeAlgorithms/TreeAlgorithms/AVL_Tree.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeAlgorithms
+{
+    class AVL_Tree<T>
+    {
+        private AVL_Node tree_Root;
+        public void Insert(T value)
+        {
+            GetInsertParent().SetLeftChild(new AVL_Node(value));
+            Sort();
+        }
+        private void Sort()
+        {
+            throw new NotImplementedException();
+            while (tree_Root.GetParent() == null)
+            {
+                tree_Root = tree_Root.GetParent();// updateing which is root
+            }
+        }
+        private AVL_Node GetInsertParent()// finds the parent to insert under
+        {
+            AVL_Node node = tree_Root;
+            while (node.GetLeftChild() == null)
+            {
+                node = node.GetLeftChild();// we need to make children and parent same class as the main class
+            }
+            return node;
+        }
+        class AVL_Node : Node<T>
+        {
+            public AVL_Node(T value) : base(value)
+            {
+            }
+
+            public bool RotateRight()
+            {
+                // function that rotates the node and its children så that left child becomes the new parent
+                if (leftChild == null) { return false; }// we need a left child to become parent, may become exception later
+                leftChild.SetParent(parent);
+                if (parent == null) { }//update root of AVL_Tree object
+                this.SetParent(leftChild);
+                this.SetLeftChild(parent.GetRightChild());
+                parent.SetRightChild(this);
+                return true;
+            }
+            public bool RotateLeft()
+            {
+                // function that rotates the node and its children så that right child becomes the new parent
+                if (rightChild == null) { return false; }// we need a right child to become parent, may become exception later
+                rightChild.SetParent(parent);
+                this.SetParent(rightChild);
+                this.SetRightChild(parent.GetLeftChild());
+                parent.SetLeftChild(this);
+                return true;
+            }
+        }
+    }
+}
---
+++ TreeAlgorithms/TreeAlgorithms/Node.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeAlgorithms
+{
+    abstract class Node<T>
+    {
+        readonly T value;
+        protected Node<T> parent;
+        protected Node<T> leftChild;
+        protected Node<T> rightChild;
+        public Node(T value)
+        {
+            this.value = value;
+        }
+        public Node<T> GetParent()
+        {
+            return parent;
+        }
+        public Node<T> GetRightChild()
+        {
+            return rightChild;
+        }
+        public Node<T> GetLeftChild()
+        {
+            return leftChild;
+        }
+        public void SetParent(Node<T> parent)
+        {
+            this.parent = parent;
+        }
+        public void SetRightChild(Node<T> child)
+        {
+            this.rightChild = child;
+        }
+        public void SetLeftChild(Node<T> child)
+        {
+            this.leftChild = child;
+        }
+
+    }
+}
